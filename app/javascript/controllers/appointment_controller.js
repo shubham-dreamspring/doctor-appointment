dd30db7efb412bc15df0eddf1b7715fea1852e4b
shubham-dreamspring <sjain@dreamspring.org>
2023-05-05 02:02:19 +0530

    Add feature of toggle disable of book appointment button

--- app/javascript/controllers/appointment_controller.js
+++ app/javascript/controllers/appointment_controller.js
@@ -2,7 +2,7 @@ import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-    static targets = ['page1', 'page2', 'toggle-btn']
+    static targets = ['page1', 'page2', 'bookButton']
 
     page_switch() {
         let page1 = this.page1Target
@@ -11,6 +11,14 @@ export default class extends Controller {
         page1.classList.add('d-none');
         page2.classList.remove('d-none')
         page2.classList.add('d-flex');
+
+    }
+
+    toggleBookBtn() {
+        let btn = this.bookButtonTarget
+        btn.disabled = false
+        btn.classList.add('btn-primary')
+        btn.classList.remove('btn-secondary')
 
     }
 }

d92e82ffaf8e76eaee7fd99c9a163e88f94adac9
shubham-dreamspring <sjain@dreamspring.org>
2023-05-28 15:55:35 +0530

    Add Appointment model specs

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,7 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :user
+
+  validates :start_timestamp,:end_timestamp, :user_id, :doctor_id, :amount, presence: true
+  validates :amount, numericality: {greater_than_or_equal_to: 0.01}
 end
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "attributes should be valid" do
+    it "start_timestamp, end_timestamp, user_id, doctor_id, amount can't be empty" do
+      doctor = Appointment.new({ user_id: 1 })
+      expect(doctor.invalid?).to be_truthy
+    end
+    it "amount should be greater than 0" do
+      doctor = Appointment.new({ start_timestamp: Date.new, end_timestamp: Date.new + 1.hour, user_id: 1, doctor_id: 2, amount: -1 })
+      expect(doctor.invalid?).to be_truthy
+    end
+  end
 end
